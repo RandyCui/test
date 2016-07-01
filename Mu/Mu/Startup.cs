7a71c85ba6bbe8294b2b9d3168fe16772309b64f
Randy Cui <Randy.Cui@synrevoice.com>
2016-07-01 13:27:20 +0800

    Add mu project

---
+++ Mu/Mu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mu.Startup))]
+namespace Mu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
